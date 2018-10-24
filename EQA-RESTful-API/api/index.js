81d58423883a4dea3687b228ec3fa43c513ce58e
RTANC <wongsathon_n@rtanc.ac.th>
2018-10-25 06:32:35 +0700

    remove google lib & modified user model

--- EQA-RESTful-API/api/index.js
+++ EQA-RESTful-API/api/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const auth = require('./auth')
+
 const user = require('./user')
 
-router.use('/auth', auth)
 router.use('/users', user)
 
 module.exports = router
--- EQA-RESTful-API/api/user/user.model.js
+++ EQA-RESTful-API/api/user/user.model.js
@@ -3,6 +3,20 @@ const userSchema = mongoose.Schema({
     username: String,
     password: String,
     fullname: String,
+    role: {
+        admin: {
+            type: Boolean,
+            default: true
+        },
+        qa: {
+            type: Boolean,
+            default: true
+        },
+        writer: {
+        },
+        checker: {
+        }
+    },
     chpasswd: {
         type: Boolean,
         default: false
